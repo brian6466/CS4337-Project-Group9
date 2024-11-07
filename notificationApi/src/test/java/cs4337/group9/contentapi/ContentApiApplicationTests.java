3630778d8c63bb99af50eef854324a5262a9744a
BrownBadger221 <rahulalam72@gmail.com>
2024-11-07 15:07:41 +0000

    Blog Class

---
+++ notificationApi/src/test/java/cs4337/group9/contentapi/ContentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cs4337.group9.contentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
