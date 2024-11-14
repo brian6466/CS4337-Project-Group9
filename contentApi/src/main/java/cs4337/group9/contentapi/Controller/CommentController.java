e16c1110e3937224f53311f19c0e4a93c16ac12c
Craig_ <21340633@studentmail.ul.ie>
2024-11-14 19:19:06 +0000

    createComment endpoint WIP for getComment getAllComments updateComment and deleteComment

--- contentApi/src/main/java/cs4337/group9/contentapi/Controller/CommentController.java
+++ contentApi/src/main/java/cs4337/group9/contentapi/Controller/CommentController.java
@@ -1,10 +1,28 @@
 package cs4337.group9.contentapi.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cs4337.group9.contentapi.Entity.CommentEntity;
+import cs4337.group9.contentapi.Service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/comment")
+@RequestMapping("/article/{articleId}/comment")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentEntity> createComment(
+            @PathVariable Long articleId,
+            @RequestParam UUID userId,
+            @RequestBody String content) {
+        CommentEntity comment = commentService.createComment(articleId, userId, content);
+        return ResponseEntity.ok(comment);
+    }
+
 
 }
