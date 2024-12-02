bceef43af9b40d111d884369ea39940a2cf67c65
brian <21326614@studentmail.ul.ie>
2024-12-02 19:41:36 +0000

    Update to non depreciated way of disabling CSRF

--- userApi/src/main/java/cs4337/group9/mediumwebsite/Config/SecurityConfig.java
+++ userApi/src/main/java/cs4337/group9/mediumwebsite/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/user").permitAll()
                         .requestMatchers("/user/ban/**", "/user/unban/**").hasAuthority("ADMIN")
