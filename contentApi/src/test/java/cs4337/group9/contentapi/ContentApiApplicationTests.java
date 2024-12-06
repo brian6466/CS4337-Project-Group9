c5f3448bd155f8f3cb578e4621eb1770584a873c
brian <21326614@studentmail.ul.ie>
2024-12-06 01:28:11 +0000

    Added running tests to pipeline (currently no tests for userapi and authapi in main so might fail before those merge)

--- contentApi/src/test/java/cs4337/group9/contentapi/ContentApiApplicationTests.java
+++ contentApi/src/test/java/cs4337/group9/contentapi/ContentApiApplicationTests.java
@@ -1,10 +1,24 @@
 package cs4337.group9.contentapi;
 
+import cs4337.group9.contentapi.Repository.ArticleRepository;
+import cs4337.group9.contentapi.Repository.CommentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 class ContentApiApplicationTests {
+
+    @MockBean
+    private ArticleRepository articleRepository;
+
+    @MockBean
+    private CommentRepository commentRepository;
+
+
 
     @Test
     void contextLoads() {
